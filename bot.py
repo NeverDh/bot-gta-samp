89022e78827547e848ddbbf5492083a4bda23838
NeverDh <luisgabriel20000@outlook.com>
2022-02-27 23:16:03 -0300

    versão 1.0

---
+++ bot.py
@@ -0,0 +1,31 @@
+import pyautogui
+import time
+
+def andar():
+    time.sleep(10)
+    pyautogui.keyDown('w')
+    pyautogui.keyDown('a')
+    pyautogui.keyUp('w')
+    pyautogui.keyDown('s')
+    pyautogui.keyUp('a')
+    pyautogui.keyDown('d')
+    pyautogui.keyUp('s')
+    pyautogui.keyDown('w')
+    pyautogui.keyUp('d')
+    pyautogui.keyUp('w')
+    time.sleep(5)
+    caixinha()
+
+
+
+def caixinha():
+    pyautogui.hotkey('t')
+    time.sleep(1)
+    pyautogui.write('/caixinha')
+    time.sleep(1)
+    pyautogui.hotkey('enter')
+    time.sleep(300)
+    andar()
+
+
+andar()
